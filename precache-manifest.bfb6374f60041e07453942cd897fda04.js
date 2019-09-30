2ea284785e7ada2b9bc0e17e0783671798ee5945
Clauber Oliveira <claubs@Claubers-MacBook-Pro.local>
2019-09-30 13:20:27 -0600

    Updates

---
+++ precache-manifest.bfb6374f60041e07453942cd897fda04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a2a56c71e8c8d968174c5f191358ae",
+    "url": "/react-movie-checkr/index.html"
+  },
+  {
+    "revision": "79f532cdf375c8c871ea",
+    "url": "/react-movie-checkr/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f2124a634f5fe850b4cd",
+    "url": "/react-movie-checkr/static/css/main.03575eef.chunk.css"
+  },
+  {
+    "revision": "79f532cdf375c8c871ea",
+    "url": "/react-movie-checkr/static/js/2.deb0cff6.chunk.js"
+  },
+  {
+    "revision": "f2124a634f5fe850b4cd",
+    "url": "/react-movie-checkr/static/js/main.893d033f.chunk.js"
+  },
+  {
+    "revision": "295c80272d0f931d6a1f",
+    "url": "/react-movie-checkr/static/js/runtime-main.7f11421f.js"
+  }
+]);
