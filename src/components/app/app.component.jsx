3ac27364226fc59b2b336d469b030bed4d8689d1
Clauber Oliveira <claubs@Claubers-MacBook-Pro.local>
2019-09-30 13:30:17 -0600

    Fixed description and margin

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import {SearchBox} from'../search-box/search-box.component'
 import {CarouselList} from '../carousel-list/carousel-list.component'
-import './app.style.scss'
+import './app.style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component{
@@ -43,21 +43,21 @@ class App extends React.Component{
     render(){
         // console.log(this.state)
         return(
-            <div>
+            <div id="movie-checkr">
                 <SearchBox placeholder='Search for a movie/actor/producer' value={this.state.searchValue} onChange={(e) => this.onInputChange(e)}/>
-                <div className="container">
+                <div className="container movie-container">
                     <div id="popular-movies" >
                         <h3>Popular Movies</h3>
                         <CarouselList items ={this.state.popularMovies.results}/>
                     </div>
                 </div>
-                <div className="container">
+                <div className="container movie-container">
                     <div id="upcoming-movies" >
                     <h3>Upcoming Movies</h3>
                     <CarouselList items ={this.state.upcomingMovies.results}/>
                     </div>
                 </div>
-                <div className="container">
+                <div className="container movie-container">
                     <div id="upcoming-movies" >
                     <h3>Now Playing</h3>
                     <CarouselList items ={this.state.nowPlaying.results}/>
