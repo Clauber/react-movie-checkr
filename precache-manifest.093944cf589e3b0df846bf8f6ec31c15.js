14b5fbd591caee34a4cfc5f0261071c26a715497
Clauber Oliveira <claubs@Claubers-MacBook-Pro.local>
2019-10-02 22:00:45 -0600

    Updates

--- precache-manifest.093944cf589e3b0df846bf8f6ec31c15.js
+++ precache-manifest.093944cf589e3b0df846bf8f6ec31c15.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bdfa9a4a7a742e0daad3a76426eb8f11",
+    "revision": "924534ede513494fade5bbd3055cedb5",
     "url": "/react-movie-checkr/index.html"
   },
   {
-    "revision": "2c100c49eed19f9d676a",
+    "revision": "955fa480e862dd8efb77",
     "url": "/react-movie-checkr/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "243f9d76e7d47639edc5",
+    "revision": "1e2a3851f3e612325bba",
     "url": "/react-movie-checkr/static/css/main.d9e19840.chunk.css"
   },
   {
-    "revision": "2c100c49eed19f9d676a",
-    "url": "/react-movie-checkr/static/js/2.2e2cba10.chunk.js"
+    "revision": "955fa480e862dd8efb77",
+    "url": "/react-movie-checkr/static/js/2.a2bde438.chunk.js"
   },
   {
-    "revision": "243f9d76e7d47639edc5",
-    "url": "/react-movie-checkr/static/js/main.7499bc77.chunk.js"
+    "revision": "1e2a3851f3e612325bba",
+    "url": "/react-movie-checkr/static/js/main.4ab0da14.chunk.js"
   },
   {
     "revision": "295c80272d0f931d6a1f",
